3e1ab0abcdabb0a93c4becbf7f3ff35f77fe906c
weber <weber50432@gmail.com>
2022-03-18 16:22:55 +0800

    Merge branch 'feature/test_feature' into develop

---
+++ pipe/src/app/test/test.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <h2>{{name|lowercase}}</h2>
+    <h2>{{name|uppercase}}</h2>
+  `,
+  styles: []
+})
+export class TestComponent implements OnInit {
+
+  public name = "Codevolution";
+  public message = "Welcome to codevolution";
+  public person = {
+    "firstName" : "John",
+    "lastName": "Doe"
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
